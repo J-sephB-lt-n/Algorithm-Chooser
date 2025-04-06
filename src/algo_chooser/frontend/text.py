f46d3625d964c19c25ae04ce7c26cdf33f4efd3b
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-04-06 21:51:11 +0200

    feat(frontend/text.py): added emoji for each applicability label

---
+++ src/algo_chooser/frontend/text.py
@@ -0,0 +1,13 @@
+"""
+Functions concerning text in the UI
+"""
+
+applicability_emoji_map: dict[str, str] = {
+    # An emoji for each possible applicability label #
+    "perfect fit": "🥇",
+    "highly applicable": "🟩",
+    "applicable": "🆗",
+    "possibly applicable": "❓",
+    "not applicable": "❌",
+    "ERROR": "⚠️",
+}
