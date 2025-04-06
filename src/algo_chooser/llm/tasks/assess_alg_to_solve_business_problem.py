ea07e85e3321a12fbf422fc440ea4c837052831b
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-04-06 21:50:28 +0200

    feat(assess_alg_to_solve_business_problem.py): changing formatting to improve LLM performance

--- src/algo_chooser/llm/tasks/assess_alg_to_solve_business_problem.py
+++ src/algo_chooser/llm/tasks/assess_alg_to_solve_business_problem.py
@@ -12,13 +12,14 @@ from src.algo_chooser.llm.client import LLMClient
 from src.algo_chooser.llm import prompts
 
 
-class AlgorithmAssessment(pydantic.BaseModel):
+class AlgAssessment(pydantic.BaseModel):
     discussion: str = pydantic.Field(
         ...,
         description="Brief discussion of the applicability of the algorithm to \
 mitigating the specific business problem, including ways it would likely be \
-applied, evidence for and against its use in this particular context, and how \
-likely it would be to generate significant value if applied to this problem.",
+applied, evidence for and against its use in this particular context, likely \
+blockers, implementation requirements, and predictions of likely value and cost. Format \
+your response as markdown with named sections.",
     )
     applicability: Literal[
         "perfect fit",
@@ -26,7 +27,20 @@ likely it would be to generate significant value if applied to this problem.",
         "applicable",
         "possibly applicable",
         "not applicable",
-    ]
+        "ERROR",
+    ] = pydantic.Field(
+        ...,
+        description="""
+| Label               | Description
+|---------------------|-------------------------------
+| perfect fit         | This is a typical/textbook use-case for this algorithm
+| highly applicable   | There is a high likelihood of success and return on investment
+| applicable          | This looks like a good use of the algorithm
+| possibly applicable | There may be a way to use this algorithm for this application
+| not applicable      | It is unlikely that this algorithm can provide value for this use-case
+| ERROR               | reserved for internal use  
+        """.strip()
+    )
 
 
 def assess_alg_to_solve_business_problem(
@@ -34,7 +48,7 @@ def assess_alg_to_solve_business_problem(
     alg_name: str,
     alg_summary: str,
     business_problem_desc: str,
-) -> AlgorithmAssessment:
+) -> AlgAssessment:
     """
     TODO
     """
@@ -42,7 +56,7 @@ def assess_alg_to_solve_business_problem(
         alg_name=alg_name,
         alg_summary=alg_summary,
         business_problem_desc=business_problem_desc,
-        output_schema=json.dumps(AlgorithmAssessment.model_json_schema()),
+        output_schema=json.dumps(AlgAssessment.model_json_schema(), indent=4),
     )
     llm_response_raw: str = llm.chat(
         messages=[
@@ -63,4 +77,4 @@ def assess_alg_to_solve_business_problem(
         raise ValueError("Could not find a JSON markdown code block in LLM response")
     json_str: str = find_json.group(1).strip()
 
-    return AlgorithmAssessment.model_validate_json(json_str)
+    return AlgAssessment.model_validate_json(json_str)
